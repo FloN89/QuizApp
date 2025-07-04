678d3d5e3d4dd354c3dc98cc2519df1da3471e05
Florian Narr <f.narr@gmx.de>
2025-07-04 23:54:29 +0200

    add js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-let questions = []
+let questions = [
+    {
+    "question": "Frage1",
+    "answer_1": "Antwort1",
+    "answer_2": "Antwort2",
+    "answer_3": "Antwort3",
+    "answer_4": "Antwort4",
+    "right_answer": 3
+    },
+
+     {
+    "question": "Frage2",
+    "answer_1": "Antwort1",
+    "answer_2": "Antwort2",
+    "answer_3": "Antwort3",
+    "answer_4": "Antwort4",
+    "right_answer": 3
+    },
+
+     {
+    "question": "Frage3",
+    "answer_1": "Antwort1",
+    "answer_2": "Antwort2",
+    "answer_3": "Antwort3",
+    "answer_4": "Antwort4",
+    "right_answer": 3
+    },
+
+     {
+    "question": "Frage4",
+    "answer_1": "Antwort1",
+    "answer_2": "Antwort2",
+    "answer_3": "Antwort3",
+    "answer_4": "Antwort4",
+    "right_answer": 3
+    },
+
+     {
+    "question": "Frage5",
+    "answer_1": "Antwort1",
+    "answer_2": "Antwort2",
+    "answer_3": "Antwort3",
+    "answer_4": "Antwort4",
+    "right_answer": 3
+    },
+];
