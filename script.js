d8c7d71050ad2c5a9acb9beef1ce392b524a90b7
Florian Narr <f.narr@gmx.de>
2025-07-05 00:29:07 +0200

    update js

--- script.js
+++ script.js
@@ -43,4 +43,30 @@ let questions = [
     "answer_4": "Antwort4",
     "right_answer": 3
     },
+     {
+    "question": "Frage6",
+    "answer_1": "Antwort1",
+    "answer_2": "Antwort2",
+    "answer_3": "Antwort3",
+    "answer_4": "Antwort4",
+    "right_answer": 3
+    },
 ];
+
+let currentQuestion = 0;
+
+function init(){
+    document.getElementById('all_questions').innerHTML = questions.length;
+    showQuestion()
+}
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+
+    document.getElementById('questionText').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+}
